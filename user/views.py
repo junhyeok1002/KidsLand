7a3d6bb67fe06e597eeea913e5e24b3498dd7e66
junhyeok1002 <chrisgold@naver.com>
2023-12-09 02:36:25 +0900

    feat(views.py): 편의상 인증번호 패스 해둔 것 해제

--- user/views.py
+++ user/views.py
@@ -245,11 +245,10 @@ class Phone_Verification(APIView):
 
         # 잘못된 번호입니다 처리 가능할듯?? -> 추후 리팩토링
 
-        # 임시수정 : 폰인증
-        # send_response = requests.post(SEND_URL, data=sms_data)  # 요청을 던지는 URL, 현재는 문자보내기
-        # if send_response.json()['message'] != 'success':
-        #     print(send_response.json())
-        #     return Response(status=500)
+        send_response = requests.post(SEND_URL, data=sms_data)  # 요청을 던지는 URL, 현재는 문자보내기
+        if send_response.json()['message'] != 'success':
+            print(send_response.json())
+            return Response(status=500)
 
         return Response(response_data, status=200)
 
@@ -293,9 +292,8 @@ class Phone_Message(APIView):  # 클래스의 post함수가 너무 뚱뚱해서 
                         # 'testmode_yn' : '' #테스트모드 적용 여부 Y/N
                         }
 
-            # 임시수정 : 폰인증
-            # send_response = requests.post(SEND_URL, data=sms_data)  # 요청을 던지는 URL, 현재는 문자보내기
-            # print(send_response.json())
+            send_response = requests.post(SEND_URL, data=sms_data)  # 요청을 던지는 URL, 현재는 문자보내기
+            print(send_response.json())
 
             # 현재 타임 스탬프를 년-월-일 시:분:초.마이크로초 형식으로 포맷팅합니다.
             korea_timezone = pytz.timezone("Asia/Seoul")  # 한국 시간대를 설정
@@ -364,9 +362,6 @@ class Check_Security_Number(APIView):
         # TODO: 실제 인증번호 확인 로직을 구현
         # 여기서는 간단하게 입력된 인증번호와 특정 값과의 비교
         success = (input_security_number == security_number)
-
-        # 인증번호 수정
-        success = "success"
 
         # 인증 결과를 JSON 응답으로 전송
         return JsonResponse({'success': success})
